73cbd6979c9d167d5c7bb998b59ee04567c4cf48
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-01-23 23:59:29 -0300

    Feat: first commit

---
+++ frontend/rss-feeder/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 underline">
+        Vite + React + Tailwind v4!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
