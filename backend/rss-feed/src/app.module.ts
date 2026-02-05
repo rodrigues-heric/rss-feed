ed0d63573945190ef8f9ff35ed0f2bf8ed3bf5cc
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-02-05 16:18:50 -0300

    Feat: saving url feed for user (#22)

--- backend/rss-feed/src/app.module.ts
+++ backend/rss-feed/src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { FeedSourceModule } from './modules/feed-source/feed-source.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule } from '@nestjs/config/dist/config.module';
     RssModule,
     AuthModule,
     UsersModule,
+    FeedSourceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/rss-feed/src/modules/rss/rss.controller.ts
+++ backend/rss-feed/src/modules/rss/rss.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -6,35 +7,41 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { RssService } from './rss.service';
-
-interface SuccessResponse {
-  success: boolean;
-  message: string;
-}
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { TokenRefreshInterceptor } from 'src/common/interceptors/token-refresh.interceptor';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { CreateRssDto } from './dto/create-rss.dto';
+import type { PostRssResponse } from './interfaces/post-rss-response.interface';
 
 @Controller('rss')
+@UseGuards(JwtAuthGuard)
+@UseInterceptors(TokenRefreshInterceptor)
 export class RssController {
   constructor(private readonly rssService: RssService) {}
 
-  @Post('feed')
+  @Post('subscribe')
   @HttpCode(HttpStatus.CREATED)
-  public async postFeedUrl(): Promise<SuccessResponse> {
-    console.log('Posting feed url');
-    return { success: true, message: 'Feed posted successfully' };
+  public async postSubscribeUrl(
+    @GetUser('sub') userId: string,
+    @Body() createRssDto: CreateRssDto,
+  ): Promise<PostRssResponse> {
+    return this.rssService.addFeedToUser(userId, createRssDto.url);
   }
 
   @Get('feeds')
   @HttpCode(HttpStatus.OK)
-  public async getFeeds(): Promise<SuccessResponse> {
+  public async getFeeds(): Promise<any> {
     console.log('Getting feeds');
     return { success: true, message: 'Feeds fetched successfully' };
   }
 
   @Delete('feed/:id')
   @HttpCode(HttpStatus.OK)
-  public async deleteFeed(@Param('id') id: string): Promise<SuccessResponse> {
+  public async deleteFeed(@Param('id') id: string): Promise<any> {
     console.log(`Deleting feed with id: ${id}`);
     return { success: true, message: 'Feed deleted successfully' };
   }
