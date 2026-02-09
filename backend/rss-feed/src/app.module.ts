2811ebedc971f8350bdad1dce4097622bbcf671a
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-02-09 17:22:21 -0300

    Feat: adding scheduler for news

--- backend/rss-feed/src/app.module.ts
+++ backend/rss-feed/src/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { FeedSourceModule } from './modules/feed-source/feed-source.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
     MongooseModule.forRoot(
       process.env.MONGO_URI ?? 'mongodb://localhost:27017/rss_aggregator',
     ),
