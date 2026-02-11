e95793551c19d30ade2bef19bad77f98d76fdcfe
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-02-11 14:50:14 -0300

    Delete: removing template

--- frontend/rss-feed/src/App.tsx
+++ frontend/rss-feed/src/App.tsx
@@ -1,11 +1,5 @@
 import type { JSX } from "react";
 
 export default function App(): JSX.Element {
-  return (
-    <div className="flex h-screen items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-sky-400 underline">
-        Vite + React + Tailwind v4!
-      </h1>
-    </div>
-  );
+  return <></>;
 }
