af018e8e1174db3e9acc2167e8e2cb5c31b36ca7
Heric Leite Rodrigues <dev.heric.rodrigues@gmail.com>
2026-03-02 20:39:45 -0300

    Update: fetching env vars from file

--- backend/rss-feed/src/app.module.ts
+++ backend/rss-feed/src/app.module.ts
@@ -8,14 +8,19 @@ import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { FeedSourceModule } from './modules/feed-source/feed-source.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
-    MongooseModule.forRoot(
-      process.env.MONGO_URI ?? 'mongodb://localhost:27017/rss_aggregator',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
     RssModule,
     AuthModule,
     UsersModule,
